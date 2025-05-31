5fa17b86d124f23cffff39a3d83e7c070ba9f4fa
ovetganna <eovetganna@icloud.com>
2025-05-31 21:19:35 +0300

    new update

--- blog_users/forms.py
+++ blog_users/forms.py
@@ -3,7 +3,7 @@ from blog_users.models import BlogUser
 from my_blog.forms import StyleFormMixin
 
 
-class BllogUserRegisterForm(StyleFormMixin, UserCreationForm):
+class BlogUserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = BlogUser
         fields = ('email', 'password1', 'password2',)
