a903b35c95d33cba738ed6afd8ee406ff7387238
ovetganna <eovetganna@icloud.com>
2025-06-09 17:08:19 +0300

    new update

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -8,7 +8,7 @@ app_name = MyBlogConfig.name
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
-    path('category/<int:pk/category_list/', BlogCategoryListView.as_view(), name='category_list'),
+    path('categories/', BlogCategoryListView.as_view(), name='categories'),
     path('blog/<int:pk>/', cache_page(60)(BlogDetailView.as_view()), name='blog_detail'),
     path('blog/create/', BlogCreateView.as_view(), name="blog_create"),
     path('blog/<int:pk>/update/', BlogUpdateView.as_view(), name="blog_update"),
